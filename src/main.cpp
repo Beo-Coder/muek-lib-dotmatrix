cbbe6f3a7cfe6827c89e10f1d0460a630a80cb28
Beo <beo-coder@protonmail.com>
2023-02-05 17:30:03 +0100

    removed explanation in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,63 +3,6 @@
 #include "mbed.h"
 #include "DOTMATRIX.h"
 
-/*
- * Script that displays a text on 8x8 led dot matrix with MAX7219 interface
- * Speed, number of displays and brightness of displays can be set
- * Max number of characters is 500 (could be increased, but would not be so smart)
- *
- *
- * Input (in variable "text") -> Output (on display)
- * A -> A
- * B -> B
- * .
- * .
- * .
- * y -> y
- * z -> z
- * 0 -> 0
- * .
- * .
- * .
- * 9 -> 9
- * *space* -> *Small space (4 width)*
- * ( -> (
- * ) -> )
- * . -> .
- * , -> ,
- * _ -> _
- * - -> -
- * ? -> ?
- * ! -> !
- *
- * special characters (with % in front (e.g. %A -> Ä)
- * A -> Ä
- * a -> ä
- * .
- * .
- * .
- * ü -> ü
- *
- * *space* -> *Normal space (8 width)*
- * 1 -> *one blank vertical line*
- * 2 -> *two blank vertical lines*
- * 3 -> *three blank vertical lines*
- * | -> *one filled vertical line*
- *
- *
- * How to add characters?
- *
- * 1. Declare character as uint8_t array with specific name (bitcode must consists of  8 elements, 9th element is the length of the char (max 8))
- * 2. Append the character that will represent the char to either alphabetSeries or specialCharsSeries (Call with % iin front of character in inputText)
- * 3. Append the char name to Alphabet (If in specialCharsSeries, at the End. If in alphabetSeries, before the first specialCharsSeries character and after the last alphabetSeries character)
- * 4. Add it to inputText
- *
- */
-
-
-
-
-
 //BEGIN OF CONFIG
 unsigned int scrollSpeed = 40;//Speed of scrolling
 const unsigned int displays = 26;//number of 8x8 displays
@@ -70,10 +13,6 @@ string inputText = "GDS1 Informatik ist das beste Profilfach der Schule %H% % % 
 //END OF CONFIG
 
 
-
-
-
-
 int main() {
 
     DOTMATRIX matrix(PC_2, PC_0, PC_1, displays, reverseDisplay);
@@ -81,15 +20,10 @@ int main() {
     matrix.setIntensity(-1, displayBrightness);
     matrix.createIDArray(inputText);
 
-    while(true){
+    while (true) {
         matrix.update();
         thread_sleep_for(scrollSpeed);
     }
-
-
-
-
-
 
 
 }
